6ac6c4683883a552acbed1c80d70c3d63089b858
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-01 17:12:06 +0700

    Detail Course - Add new Basic Activity with Home as parent

---
+++ app/src/main/java/io/github/fatimazza/academyapp/DetailCourseActivity.kt
@@ -0,0 +1,23 @@
+package io.github.fatimazza.academyapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_detail_course.*
+
+class DetailCourseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_course)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
