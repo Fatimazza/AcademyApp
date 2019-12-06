1de3dcc51b1016cd080ac97325ec80c9587fef5d
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 21:02:32 +0700

    Detail Course - Add Initial Adapter

---
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/detail/DetailCourseAdapter.kt
@@ -0,0 +1,12 @@
+package io.github.fatimazza.academyapp.ui.detail
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class DetailCourseAdapter {
+
+    inner class DetailCourseViewHolder(private val view: View)
+        : RecyclerView.ViewHolder(view) {
+
+    }
+}
