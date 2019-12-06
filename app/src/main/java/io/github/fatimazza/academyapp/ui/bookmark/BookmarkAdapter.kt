622b620f0afc98951ac558a9f9e344add7431700
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 18:40:50 +0700

    Bookmark - Add Initial Adapter

---
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/bookmark/BookmarkAdapter.kt
@@ -0,0 +1,11 @@
+package io.github.fatimazza.academyapp.ui.bookmark
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class BookmarkAdapter {
+
+    inner class BookmarkViewHolder(private val view:View): RecyclerView.ViewHolder(view) {
+
+    }
+}
