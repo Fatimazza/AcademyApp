83396ac3ad72407829b9c77c74f00a075ca5ef86
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 18:50:25 +0700

    Bookmark - In Adapter View Holder, Show all data

--- app/src/main/java/io/github/fatimazza/academyapp/ui/bookmark/BookmarkAdapter.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/bookmark/BookmarkAdapter.kt
@@ -4,8 +4,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import io.github.fatimazza.academyapp.R
 import io.github.fatimazza.academyapp.data.CourseEntity
+import kotlinx.android.synthetic.main.item_list_bookmark.view.*
 
 class BookmarkAdapter : RecyclerView.Adapter<BookmarkAdapter.BookmarkViewHolder>() {
 
@@ -28,7 +31,24 @@ class BookmarkAdapter : RecyclerView.Adapter<BookmarkAdapter.BookmarkViewHolder>
     inner class BookmarkViewHolder(private val view:View): RecyclerView.ViewHolder(view) {
 
         fun bind(course: CourseEntity, position: Int) {
+            with(view) {
+                tv_bookmark_title.text = course.title
+                tv_bookmark_date.text = course.deadline
+                tv_bookmark_description.text = course.description
+                setOnClickListener {
 
+                }
+                iv_bookmark_share.setOnClickListener {
+
+                }
+
+                Glide.with(this.context)
+                    .load(course.imagePath)
+                    .apply(RequestOptions()
+                        .placeholder(R.drawable.ic_loading)
+                        .error(R.drawable.ic_error))
+                    .into(iv_bookmark_poster)
+            }
         }
     }
 }
