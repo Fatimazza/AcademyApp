b4d73053e007adf6ba5cc2907be85ca3bb03cd37
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 16:58:03 +0700

    Academy - Add Initial Adapter

---
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/academy/AcademyAdapter.kt
@@ -0,0 +1,11 @@
+package io.github.fatimazza.academyapp.ui.academy
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class AcademyAdapter {
+
+    class AcademyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+}
