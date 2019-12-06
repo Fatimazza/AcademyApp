7eb142db425f994cbda635a10c910920a74f8419
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 21:03:43 +0700

    Detail Course - In Adapter, Implement its View Holder

--- app/src/main/java/io/github/fatimazza/academyapp/ui/detail/DetailCourseAdapter.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/detail/DetailCourseAdapter.kt
@@ -1,9 +1,22 @@
 package io.github.fatimazza.academyapp.ui.detail
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class DetailCourseAdapter {
+class DetailCourseAdapter: RecyclerView.Adapter<DetailCourseAdapter.DetailCourseViewHolder>() {
+    
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailCourseViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: DetailCourseViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     inner class DetailCourseViewHolder(private val view: View)
         : RecyclerView.ViewHolder(view) {
