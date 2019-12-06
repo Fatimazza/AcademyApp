b89ebbafa83979c4a2547ace4392909c15f2c9b6
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 18:42:30 +0700

    Bookmark - In Adapter, Implement its View Holder

--- app/src/main/java/io/github/fatimazza/academyapp/ui/bookmark/BookmarkAdapter.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/bookmark/BookmarkAdapter.kt
@@ -1,9 +1,22 @@
 package io.github.fatimazza.academyapp.ui.bookmark
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class BookmarkAdapter {
+class BookmarkAdapter : RecyclerView.Adapter<BookmarkAdapter.BookmarkViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookmarkViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: BookmarkViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     inner class BookmarkViewHolder(private val view:View): RecyclerView.ViewHolder(view) {
 
