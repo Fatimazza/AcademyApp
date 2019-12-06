348ba5ddc5d59c204c009632812c1ae0a7362b3c
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 22:07:24 +0700

    Module List Reader - Add initial Adapter

---
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/reader/list/ModuleListAdapter.kt
@@ -0,0 +1,11 @@
+package io.github.fatimazza.academyapp.ui.reader.list
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ModuleListAdapter {
+
+    inner class ModuleListViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+
+    }
+}
