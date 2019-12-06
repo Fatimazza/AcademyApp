55d6ca32152d693fb3b90699d9f1f517914be131
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 16:59:32 +0700

    Academy - In Adapter, Implement its View Holder

--- app/src/main/java/io/github/fatimazza/academyapp/ui/academy/AcademyAdapter.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/academy/AcademyAdapter.kt
@@ -1,9 +1,22 @@
 package io.github.fatimazza.academyapp.ui.academy
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class AcademyAdapter {
+class AcademyAdapter: RecyclerView.Adapter<AcademyAdapter.AcademyViewHolder>() {
+    
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AcademyViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: AcademyViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     class AcademyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
