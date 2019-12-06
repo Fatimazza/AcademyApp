5ab0877ea364980421b0c9e6e16da2fae98755d3
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 17:31:34 +0700

    Academy - In Adapter, Implement needed Functions

--- app/src/main/java/io/github/fatimazza/academyapp/ui/academy/AcademyAdapter.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/academy/AcademyAdapter.kt
@@ -1,24 +1,33 @@
 package io.github.fatimazza.academyapp.ui.academy
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import io.github.fatimazza.academyapp.R
+import io.github.fatimazza.academyapp.data.CourseEntity
+import io.github.fatimazza.academyapp.ui.academy.AcademyAdapter.AcademyViewHolder
 
-class AcademyAdapter: RecyclerView.Adapter<AcademyAdapter.AcademyViewHolder>() {
-    
+class AcademyAdapter(val listCourse: ArrayList<CourseEntity>): RecyclerView.Adapter<AcademyViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AcademyViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list_academy, parent, false)
+        return AcademyViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return listCourse.size
     }
 
     override fun onBindViewHolder(holder: AcademyViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.bind(listCourse[position], position)
     }
 
-    class AcademyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    inner class AcademyViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
 
+        fun bind(course: CourseEntity, position: Int) {
+
+        }
     }
 }
