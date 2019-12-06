80437416f98165eaf23ca5ded8c58ba6c434e45b
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 18:33:53 +0700

    Academy - Setup Adapter, Show List Course in Activity

--- app/src/main/java/io/github/fatimazza/academyapp/ui/academy/AcademyFragment.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/academy/AcademyFragment.kt
@@ -6,10 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import io.github.fatimazza.academyapp.R
+import io.github.fatimazza.academyapp.utils.DataDummy
+import kotlinx.android.synthetic.main.fragment_academy.*
 
 class AcademyFragment : Fragment() {
+
+    private val rvCourse: RecyclerView
+        get() = rv_academy_list
+
+    lateinit var academyAdapter: AcademyAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,4 +28,21 @@ class AcademyFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_academy, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        setupAdapter()
+        showListCourse()
+    }
+
+    private fun setupAdapter() {
+        academyAdapter = AcademyAdapter(requireActivity())
+        academyAdapter.setData(DataDummy.generateDummyCourses())
+    }
+
+    private fun showListCourse() {
+        rvCourse.layoutManager = LinearLayoutManager(requireContext())
+        rvCourse.setHasFixedSize(true)
+        rvCourse.adapter = academyAdapter
+    }
 }
