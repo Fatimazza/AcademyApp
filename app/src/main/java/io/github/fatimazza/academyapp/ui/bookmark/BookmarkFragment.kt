47fd2e8e93a382dff1e9d4e19c0f9ad7305c80d3
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 20:50:21 +0700

    Bookmark - Setup Adapter, Show List Bookmarked Course in Activity

--- app/src/main/java/io/github/fatimazza/academyapp/ui/bookmark/BookmarkFragment.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/bookmark/BookmarkFragment.kt
@@ -7,11 +7,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ShareCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import io.github.fatimazza.academyapp.R
 import io.github.fatimazza.academyapp.data.CourseEntity
+import io.github.fatimazza.academyapp.utils.DataDummy
+import kotlinx.android.synthetic.main.fragment_bookmark.*
 
 class BookmarkFragment : Fragment(), BookmarkFragmentCallback {
+
+    private val rvBookmarkedCourse: RecyclerView
+        get() = rv_bookmark_list
+
+    lateinit var bookmarkAdapter: BookmarkAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +28,24 @@ class BookmarkFragment : Fragment(), BookmarkFragmentCallback {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bookmark, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        setupAdapter()
+        showListBookmarkedCourse()
+    }
+
+    private fun setupAdapter() {
+        bookmarkAdapter = BookmarkAdapter(requireActivity(), this)
+        bookmarkAdapter.setData(DataDummy.generateDummyCourses())
+    }
+
+    private fun showListBookmarkedCourse() {
+        rvBookmarkedCourse.layoutManager = LinearLayoutManager(requireContext())
+        rvBookmarkedCourse.setHasFixedSize(true)
+        rvBookmarkedCourse.adapter = bookmarkAdapter
     }
 
     override fun onShareClick(course: CourseEntity) {
