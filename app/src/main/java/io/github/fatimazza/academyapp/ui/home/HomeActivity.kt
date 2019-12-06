cab937ed350785cb30fec30f50582b39e71fefa8
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 20:59:03 +0700

    Home - Setup Selected Bottom Navigation Menu

--- app/src/main/java/io/github/fatimazza/academyapp/ui/home/HomeActivity.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/home/HomeActivity.kt
@@ -2,7 +2,6 @@ package io.github.fatimazza.academyapp.ui.home
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
@@ -14,6 +13,10 @@ import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
+    companion object {
+        const val SELECTED_MENU = "selected_menu"
+    }
+
     private val navView: BottomNavigationView
         get() = nav_view
 
@@ -22,6 +25,20 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         setupNavigation()
+        setupSelectedMenu(savedInstanceState)
+    }
+
+    private fun setupSelectedMenu(savedInstanceState: Bundle?) {
+        if (savedInstanceState != null) {
+            savedInstanceState.getInt(SELECTED_MENU)
+        } else {
+            navView.selectedItemId = R.id.action_home
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(SELECTED_MENU, navView.selectedItemId)
     }
 
     private fun setupNavigation() {
