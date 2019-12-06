88d2b446cb94bc6ac38876edcc265d2849140202
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 18:46:30 +0700

    Bookmark - In Adapter, Implement needed Functions

--- app/src/main/java/io/github/fatimazza/academyapp/ui/bookmark/BookmarkAdapter.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/bookmark/BookmarkAdapter.kt
@@ -1,24 +1,34 @@
 package io.github.fatimazza.academyapp.ui.bookmark
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import io.github.fatimazza.academyapp.R
+import io.github.fatimazza.academyapp.data.CourseEntity
 
 class BookmarkAdapter : RecyclerView.Adapter<BookmarkAdapter.BookmarkViewHolder>() {
 
+    private var listCourse = arrayListOf<CourseEntity>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookmarkViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list_bookmark, parent, false)
+        return BookmarkViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return listCourse.size
     }
 
     override fun onBindViewHolder(holder: BookmarkViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.bind(listCourse[position], position)
     }
 
     inner class BookmarkViewHolder(private val view:View): RecyclerView.ViewHolder(view) {
 
+        fun bind(course: CourseEntity, position: Int) {
+
+        }
     }
 }
