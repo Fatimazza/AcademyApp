ffa1c8351dd92112fa61b47315fc49f8d7040ccc
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 17:37:40 +0700

    Academy - In Adapter View Holder, Show all data

--- app/src/main/java/io/github/fatimazza/academyapp/ui/academy/AcademyAdapter.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/academy/AcademyAdapter.kt
@@ -4,9 +4,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import io.github.fatimazza.academyapp.R
 import io.github.fatimazza.academyapp.data.CourseEntity
 import io.github.fatimazza.academyapp.ui.academy.AcademyAdapter.AcademyViewHolder
+import kotlinx.android.synthetic.main.item_list_academy.view.*
 
 class AcademyAdapter(val listCourse: ArrayList<CourseEntity>): RecyclerView.Adapter<AcademyViewHolder>() {
 
@@ -27,7 +30,21 @@ class AcademyAdapter(val listCourse: ArrayList<CourseEntity>): RecyclerView.Adap
     inner class AcademyViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
 
         fun bind(course: CourseEntity, position: Int) {
+            with(view) {
+                tv_course_title.text = course.title
+                tv_course_description.text = course.description
+                tv_course_date.text = String.format("Deadline %s", course.deadline)
+                setOnClickListener {
 
+                }
+
+                Glide.with(this.context)
+                    .load(course.imagePath)
+                    .apply(RequestOptions()
+                        .placeholder(R.drawable.ic_loading)
+                        .error(R.drawable.ic_error))
+                    .into(iv_course_poster)
+            }
         }
     }
 }
