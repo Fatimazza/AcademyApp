651a7c346ee1da94dcc8a3264211c7df01d56adf
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-06 21:13:47 +0700

    Model - Modify Module Data Classes

--- app/src/main/java/io/github/fatimazza/academyapp/data/ModuleEntity.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/data/ModuleEntity.kt
@@ -2,9 +2,9 @@ package io.github.fatimazza.academyapp.data
 
 data class ModuleEntity (
     val contentEntity: ContentEntity? = null,
-    private val mModuleId: String = "",
-    private val mCourseId: String = "",
-    private val mTitle: String = "",
-    private val mPosition: Int = 0,
-    private val mRead: Boolean? = false
+    val moduleId: String = "",
+    val courseId: String = "",
+    val title: String = "",
+    val position: Int = 0,
+    val read: Boolean? = false
 )
