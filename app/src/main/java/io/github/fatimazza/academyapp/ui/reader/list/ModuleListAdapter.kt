11d9f5428448eb3b2079955e10e91e9cfe1d0cb4
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-14 08:58:51 +0700

    Module List Reader - In Adapter, Implement its View Holder

--- app/src/main/java/io/github/fatimazza/academyapp/ui/reader/list/ModuleListAdapter.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/reader/list/ModuleListAdapter.kt
@@ -1,9 +1,22 @@
 package io.github.fatimazza.academyapp.ui.reader.list
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class ModuleListAdapter {
+class ModuleListAdapter: RecyclerView.Adapter<ModuleListAdapter.ModuleListViewHolder>() {
+    
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModuleListViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ModuleListViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     inner class ModuleListViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
 
