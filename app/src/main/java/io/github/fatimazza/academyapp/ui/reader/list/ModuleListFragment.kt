3f2a415ad3b7202a49fea5477a09982c291fa606
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-14 10:32:42 +0700

    Module List Reader - In Module List Fragment, Init Views

--- app/src/main/java/io/github/fatimazza/academyapp/ui/reader/list/ModuleListFragment.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/reader/list/ModuleListFragment.kt
@@ -6,11 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.recyclerview.widget.RecyclerView
 
 import io.github.fatimazza.academyapp.R
+import kotlinx.android.synthetic.main.fragment_module_list.*
 
 class ModuleListFragment : Fragment() {
 
+    private val progressModuleList: ProgressBar
+        get() = pb_module_list
+
+    private val rvModuleList: RecyclerView
+        get() = rv_module_list
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
