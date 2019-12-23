151fa37873f61fe2dc5e24d1d56e11a5483af93e
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-23 12:20:59 +0700

    Detail Course - In View Model, Add functions to get Module and Course

--- app/src/main/java/io/github/fatimazza/academyapp/ui/detail/DetailCourseViewModel.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/detail/DetailCourseViewModel.kt
@@ -1,7 +1,29 @@
 package io.github.fatimazza.academyapp.ui.detail
 
 import androidx.lifecycle.ViewModel
+import io.github.fatimazza.academyapp.data.CourseEntity
+import io.github.fatimazza.academyapp.data.ModuleEntity
+import io.github.fatimazza.academyapp.utils.DataDummy
+
 
 class DetailCourseViewModel : ViewModel() {
+
+    var courseId: String = ""
+
+    lateinit var courseEntity: CourseEntity
+
+    fun getCourse(): CourseEntity {
+        for (i in 0 until DataDummy.generateDummyCourses().size) {
+            val courseEntity = DataDummy.generateDummyCourses()[i]
+            if (courseEntity.courseId == courseId) {
+                this.courseEntity = courseEntity
+            }
+        }
+        return courseEntity
+    }
+
+    fun getModules(): List<ModuleEntity> {
+        return DataDummy.generateDummyModules(courseId)
+    }
 
 }
