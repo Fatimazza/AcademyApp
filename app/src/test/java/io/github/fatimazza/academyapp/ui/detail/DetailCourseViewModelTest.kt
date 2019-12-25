df115d24006b72b1045e6c1c6958eee6cf357e8e
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-25 15:40:02 +0700

    Detail Course Unit Test - Set up View Model and Course Entity

--- app/src/test/java/io/github/fatimazza/academyapp/ui/detail/DetailCourseViewModelTest.kt
+++ app/src/test/java/io/github/fatimazza/academyapp/ui/detail/DetailCourseViewModelTest.kt
@@ -1,10 +1,29 @@
 package io.github.fatimazza.academyapp.ui.detail
 
+import io.github.fatimazza.academyapp.data.CourseEntity
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 class DetailCourseViewModelTest {
+
+    private lateinit var viewModel: DetailCourseViewModel
+
+    private lateinit var dummyCourse: CourseEntity
+
+    @Before
+    fun setUp() {
+        viewModel = DetailCourseViewModel()
+        dummyCourse = CourseEntity(
+            "a14",
+            "Menjadi Android Developer Expert",
+            "Dicoding sebagai satu-satunya Google Authorized Training Partner di Indonesia telah melalui proses penyusunan kurikulum secara komprehensif. Semua modul telah diverifikasi langsung oleh Google untuk memastikan bahwa materi yang diajarkan relevan dan sesuai dengan kebutuhan industri digital saat ini. Peserta akan belajar membangun aplikasi Android dengan materi Testing, Debugging, Application, Application UX, Fundamental Application Components, Persistent Data Storage, dan Enhanced System Integration.",
+            "100 Hari",
+            false,
+            "https://www.dicoding.com/images/small/academy/menjadi_android_developer_expert_logo_070119140352.jpg"
+        )
+    }
 
     @Test
     fun getCourse() {
