d4d8502ced8a8a706ce017359c2a79bb3a52a2bf
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-25 15:29:39 +0700

    Bookmark Unit Test - Initial Test Class

---
+++ app/src/test/java/io/github/fatimazza/academyapp/ui/bookmark/BookmarkViewModelTest.kt
@@ -0,0 +1,12 @@
+package io.github.fatimazza.academyapp.ui.bookmark
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class BookmarkViewModelTest {
+
+    @Test
+    fun getBookmarks() {
+    }
+}
