90905adc92e466c942426847251574e8b0b44701
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-25 15:37:20 +0700

    Detail Course Unit Test - Initial Test Class

---
+++ app/src/test/java/io/github/fatimazza/academyapp/ui/detail/DetailCourseViewModelTest.kt
@@ -0,0 +1,16 @@
+package io.github.fatimazza.academyapp.ui.detail
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class DetailCourseViewModelTest {
+
+    @Test
+    fun getCourse() {
+    }
+
+    @Test
+    fun getModules() {
+    }
+}
