f86e44a8cc4904a9ca4d4386c776314f38dc286a
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-25 15:28:51 +0700

    Academy Unit Test - Add Test for Get Course

--- app/src/test/java/io/github/fatimazza/academyapp/ui/academy/AcademyViewModelTest.kt
+++ app/src/test/java/io/github/fatimazza/academyapp/ui/academy/AcademyViewModelTest.kt
@@ -3,11 +3,23 @@ package io.github.fatimazza.academyapp.ui.academy
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 class AcademyViewModelTest {
 
+    private lateinit var viewModel: AcademyViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = AcademyViewModel()
+    }
+
     @Test
     fun getCourse() {
-        
+        val courseEntities = viewModel.getCourse()
+        // make sure Course data not null
+        assertNotNull(courseEntities)
+        // make sure total of Course data is as expected
+        assertEquals(5, courseEntities.size)
     }
 }
