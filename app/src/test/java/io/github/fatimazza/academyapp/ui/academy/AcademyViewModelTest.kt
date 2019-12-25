0398b6812019d97d95938da4afed50839fd96fee
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-25 15:25:10 +0700

    Academy Unit Test - Initial Test Class

---
+++ app/src/test/java/io/github/fatimazza/academyapp/ui/academy/AcademyViewModelTest.kt
@@ -0,0 +1,13 @@
+package io.github.fatimazza.academyapp.ui.academy
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class AcademyViewModelTest {
+
+    @Test
+    fun getCourse() {
+        
+    }
+}
