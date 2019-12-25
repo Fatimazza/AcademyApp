c521a1875306757d8fcfe2d5353fe96c00d60e18
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-25 15:34:50 +0700

    Bookmark Unit Test - Add Test for Get Bookmarked Course

--- app/src/test/java/io/github/fatimazza/academyapp/ui/bookmark/BookmarkViewModelTest.kt
+++ app/src/test/java/io/github/fatimazza/academyapp/ui/bookmark/BookmarkViewModelTest.kt
@@ -3,10 +3,23 @@ package io.github.fatimazza.academyapp.ui.bookmark
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 class BookmarkViewModelTest {
 
+    private lateinit var viewModel: BookmarkViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = BookmarkViewModel()
+    }
+
     @Test
     fun getBookmarks() {
+        val bookmarkedCourseEntities = viewModel.getBookmarks()
+        // make sure Bookmarked Course data not null
+        assertNotNull(bookmarkedCourseEntities)
+        // make sure total of Bookmarked Course data is as expected
+        assertEquals(5, bookmarkedCourseEntities.size)
     }
 }
