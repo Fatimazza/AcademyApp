2643bb4293209d22d6b395221d2d156dcf38bc40
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-25 13:45:32 +0700

    Module Content Reader - In Content Fragment, Init View Model

--- app/src/main/java/io/github/fatimazza/academyapp/ui/reader/content/ModuleContentFragment.kt
+++ app/src/main/java/io/github/fatimazza/academyapp/ui/reader/content/ModuleContentFragment.kt
@@ -8,8 +8,10 @@ import android.view.ViewGroup
 import android.webkit.WebView
 import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import io.github.fatimazza.academyapp.R
 import io.github.fatimazza.academyapp.data.ContentEntity
+import io.github.fatimazza.academyapp.ui.reader.CourseReaderViewModel
 import kotlinx.android.synthetic.main.fragment_module_content.*
 
 class ModuleContentFragment : Fragment() {
@@ -24,6 +26,8 @@ class ModuleContentFragment : Fragment() {
     private val pbModuleContent: ProgressBar
         get() = pb_module_content
 
+    private lateinit var viewModel: CourseReaderViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,9 +39,14 @@ class ModuleContentFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        initViewModel()
         val entity =
             ContentEntity("<h3 class=\\\"fr-text-bordered\\\">Contoh Content</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>")
         populateWebView(entity)
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(requireActivity()).get(CourseReaderViewModel::class.java)
     }
 
     private fun populateWebView(content: ContentEntity) {
